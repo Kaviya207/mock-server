13e054eead04cccbe67d43d7a1b4839237c1259b
Kaviya207 <kaviya.rangan@kulu.com>
2025-12-11 19:03:32 +0530

    change

--- shared/db/config.js
+++ shared/db/config.js
@@ -1,10 +1,10 @@
 // const { createClient } = require("@supabase/supabase-js");
-const { createClient } = require("@supabase/supabase-js");
-require("dotenv").config();
+// const { createClient } = require("@supabase/supabase-js");
+// require("dotenv").config();
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+// const supabaseUrl = process.env.SUPABASE_URL;
+// const supabaseKey = process.env.SUPABASE_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+// const supabase = createClient(supabaseUrl, supabaseKey);
 
-module.exports = supabase;
+// module.exports = supabase;
